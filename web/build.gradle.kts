60c2c443ba177a70395e5c24441e0bf6a8deb9b7
mattiahit <mattiahit@gmail.com>
2023-01-16 23:27:17 +0100

    impl get all and save to restaurant controller

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
 	id("org.springframework.boot") version "3.0.1"
 	id("io.spring.dependency-management") version "1.1.0"
@@ -21,13 +19,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.modelmapper:modelmapper:3.1.1")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "17"
--- web/src/main/kotlin/pl/mattiahit/myrestaurant/restaurant/RestaurantController.kt
+++ web/src/main/kotlin/pl/mattiahit/myrestaurant/restaurant/RestaurantController.kt
@@ -1,11 +1,29 @@
 package pl.mattiahit.myrestaurant.restaurant
 
+import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import pl.mattiahit.myrestaurant.db.entity.Restaurant
 
 @RestController
+@RequestMapping("restaurant")
 class RestaurantController {
 
     @Autowired
     private lateinit var restaurantService: RestaurantService
+
+    private lateinit var modelMapper: ModelMapper
+
+    @GetMapping("/get-all", produces = ["application/json;charset=UTF-8"])
+    fun getAllRestaurants(): List<RestaurantDto> {
+        return restaurantService.getAllRestaurants().map { it.toDTO() }
+    }
+
+    @GetMapping("/save/{restaurant}")
+    fun saveNewRestaurant(@PathVariable restaurant: RestaurantDto) {
+        restaurantService.createRestaurant(modelMapper.map(restaurant, Restaurant::class.java))
+    }
 }
